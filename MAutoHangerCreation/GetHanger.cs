d99f9e1d626026f7d493941ea0ff8aec853b0214
郭豐瑋 郭 <fwkuo@continental-engineering.com>
2022-12-15 09:35:22 +0800

    加入專案檔案。

---
+++ MAutoHangerCreation/GetHanger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+
+
+namespace MAutoHangerCreation
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class GetHanger:IExternalCommand:{
+        public Result Execute(ExternalCommandData commandData, out string message, ElementSet elements) { 
+    }
+    }
+}
