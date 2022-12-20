e3e7e23ba8a1842fe8e8142672ca2fb64eb3e57f
郭豐瑋 郭 <fwkuo@continental-engineering.com>
2022-12-20 13:13:02 +0800

    完成抓取管吊架

--- MAutoHangerCreation/05_FilterByParameter.cs
+++ MAutoHangerCreation/05_FilterByParameter.cs
@@ -26,11 +26,31 @@ namespace MAutoHangerCreation
             LogicalAndFilter andFilter = new LogicalAndFilter(filter1, filter2);
             IList<Element> elemList = collector.WherePasses(andFilter).ToElements();
 
-            st.AppendLine("FilteredElementCollector收集到的有：");
-            
+            string paraName = "API識別名稱";
+            string targetName = "吊架";
+            List<Element> filteredByPara = new List<Element>();
 
-            
+            //自建Filter
             foreach (Element elem in elemList) {
+                Parameter para = elem.LookupParameter(paraName);
+                //在需要存取某物件的屬性之前，我們通常會先檢查該物件是否為 null，以免程式執行時拋出異常（NullReferenceException）
+                /*
+                if (para != null && para.AsString().Contains(targetName) == true) {
+                    filteredByPara.Add(elem);
+                }
+                */
+
+                //C# 6 新增了 null 條件運算子語法，讓你用更簡短的語法達到同樣效果：
+                //先判斷物件是否為 null，不是 null 才會存取其成員。它的寫法是在變數後面跟著一個問號，然後是存取成員名稱的表示式。
+                if (para?.AsString().Contains(targetName) == true)
+                {
+                    filteredByPara.Add(elem);
+                }
+            }
+
+            st.AppendLine("FilteredByParameter收集到的有：");
+            
+            foreach (Element elem in filteredByPara) {
                 Parameter para = elem.get_Parameter(BuiltInParameter.ALL_MODEL_FAMILY_NAME);
                 st.AppendLine(para.AsString() + "......" + elem.Name);
             }
