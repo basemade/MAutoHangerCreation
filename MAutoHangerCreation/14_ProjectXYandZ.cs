7e68b9dcfcf9929b054d2b740fe598fd53270ab0
郭豐瑋 郭 <fwkuo@continental-engineering.com>
2022-12-30 15:36:51 +0800

    完成pipecurve最近點修正

--- MAutoHangerCreation/14_ProjectXYandZ.cs
+++ MAutoHangerCreation/14_ProjectXYandZ.cs
@@ -16,7 +16,7 @@ namespace MAutoHangerCreation
     //在滑鼠點擊處創建Instance
 
     [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-    public class ProjectXY : IExternalCommand
+    public class ProjectXYandZ : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
@@ -25,62 +25,6 @@ namespace MAutoHangerCreation
             Document doc = uidoc.Document;
             Selection sel = uidoc.Selection;
             StringBuilder st = new StringBuilder();
-            PipeSelFilter gagaFilter = new PipeSelFilter(doc);
-
-            #region 獲取點的方式 1
-            //XYZ pt = sel.PickPoint(ObjectSnapTypes.Nearest);
-            //PickPoint方法也可以獲得點，但要求得在工作平面上才能使用(3D不行) 
-            #endregion
-
-            #region 獲取點的方式 2，step1
-            Reference selPipePtRef = sel.PickObject(ObjectType.PointOnElement, gagaFilter);
-            //reference是一群幾何描述的集合，會隨著ObjectType帶出針對目標物件的描述
-            //因此ObjectType的選擇，會影響到得到的reference
-            XYZ pt = selPipePtRef.GlobalPoint;
-            //用上面這行得到的位置是點擊Element時，當下滑鼠於Element表面的位置 
-            //Z的位置會隨著管的表面上上下下，沒有統一
-            #endregion
-
-
-            #region 獲取點的方式 2，step2 @@@@@@
-            //將Reference轉換成Element，抽取其中的
-            Element turnRefToElem = doc.GetElement(selPipePtRef.ElementId);
-            LocationCurve locaCrv = turnRefToElem.Location as LocationCurve;
-            XYZ crvEnd = locaCrv.Curve.GetEndPoint(0);
-            //為什麼element需要先叫出Location屬性，然後再 as LocationCurve?
-            //可以看看 API的Inheritance Hierarchy：https://www.revitapidocs.com/2023/3dbe57e5-fdea-5bf9-c715-52653f56073f.htm
-            //有2個class繼承Location：LocationCurve & LocationPoint
-            //API在設計時是以繼承的概念進行，假設今天有新的類別A1 A2
-            //做法上會是A1 A2都繼承A >>> A1 A2都有A的屬性
-            //而不是把A1 A2各自新的屬性和方法，在 A 裡面去修改新增
-
-            //而使用API上，因為已經過篩選，選到的element其實已經是帶有LocationCurve的特性了
-            //再者，要得到的是LocationCurve.curve.GetEndPoint()
-            //因此才需要這動作：Element.Location as LocationCurve
-            #endregion
-
-
-            #region 獲取點的方式 2，step3 @@@@@@
-            //Revit calculates system units in Imperial units (Feet and Fractional Inches). 
-            double convertZ = UnitUtils.Convert(crvEnd.Z, DisplayUnitType.DUT_DECIMAL_FEET, DisplayUnitType.DUT_MILLIMETERS);
-            //長度單位轉換 https://blog.csdn.net/ltylove2007/article/details/107214998
-            st.AppendLine(convertZ.ToString());
-            MessageBox.Show(st.ToString());
-            st.Clear(); 
-            #endregion
-
-
-            XYZ pt2 = new XYZ(pt.X, pt.Y, crvEnd.Z);
-            //用上面這行
-            @@@@@@@@@@@@@@
-            //revit lookup 裡 Location 都是基於Internal Origin
-            //由於管的Location是基於Internal Origin
-            //而要創造出來的吊架則是基於level，因此要扣掉level的elevation
-
-
-            //結果：有2個問題待解決
-            //PointOnElement得到點位是在模型上的任意位置，非中心線上
-            //並且高度不對
 
             #region 篩選：管附件+族群
             FilteredElementCollector collector = new FilteredElementCollector(doc);
@@ -148,6 +92,66 @@ namespace MAutoHangerCreation
             st.Clear();
             #endregion
 
+
+            ////////////////////////////////////////////////////////////////////////////////////////////
+            #region 獲取點的方式 1
+            //XYZ pt = sel.PickPoint(ObjectSnapTypes.Nearest);
+            //PickPoint方法也可以獲得點，但要求得在工作平面上才能使用(3D不行) 
+            #endregion
+
+            #region 獲取點的方式 2，step1：PickObject
+            PipeSelFilter gagaFilter = new PipeSelFilter(doc);
+            Reference selPipePtRef = sel.PickObject(ObjectType.PointOnElement, gagaFilter);
+            //reference是一群幾何描述的集合，會隨著ObjectType帶出針對目標物件的描述
+            //因此ObjectType的選擇，會影響到得到的reference
+            XYZ pt = selPipePtRef.GlobalPoint;
+            //The position on which the reference is hit.
+            //上面這行會得到點擊Element時，當下滑鼠於Element表面的位置 
+            //Z的位置會隨著管的表面上上下下，沒有統一
+            #endregion
+
+            #region 獲取點的方式 2，step2：找尋在管中心線上的最近點
+            //將Reference轉換成Element，抽取其中的幾何資訊
+            Element turnRefToElem = doc.GetElement(selPipePtRef.ElementId);
+            LocationCurve locaCrv = turnRefToElem.Location as LocationCurve;
+            //為什麼element需要先叫出Location屬性，然後再 as LocationCurve?
+            //可以看看 API的Inheritance Hierarchy：https://www.revitapidocs.com/2023/3dbe57e5-fdea-5bf9-c715-52653f56073f.htm
+            //有2個class繼承Location：LocationCurve & LocationPoint
+            //API在設計時是以繼承的概念進行，假設今天有新的類別A1 A2
+            //做法上會是A1 A2都繼承A >>> A1 A2都有A的屬性
+            //而不是把A1 A2各自新的屬性和方法，在 A 裡面去修改新增
+
+            //而使用API上，因為已經過篩選，選到的element其實已經是帶有LocationCurve的特性了
+            //再者，要得到的是LocationCurve.curve.xxxx()
+            //因此才需要這動作：Element.Location as LocationCurve
+            #endregion
+
+            #region 獲取點的方式 2，step2：找尋在管中心線上的最近點
+            IntersectionResult projectResult = locaCrv.Curve.Project(pt);
+            XYZ ptClosest = projectResult.XYZPoint;
+            #endregion
+
+            #region 獲取點的方式 2，step3：修正Z位置
+            //Revit calculates system units in Imperial units (Feet and Fractional Inches). 
+            //Revit lookup 裡 Location 都是基於Internal Origin
+            double convertZ = UnitUtils.Convert(ptClosest.Z, DisplayUnitType.DUT_DECIMAL_FEET, DisplayUnitType.DUT_MILLIMETERS);
+            //長度單位轉換 https://blog.csdn.net/ltylove2007/article/details/107214998
+            st.AppendLine(convertZ.ToString());
+            MessageBox.Show(st.ToString());
+            st.Clear();
+
+            //雖然得到了管的Location(基於Internal Origin)
+            //但創造吊架的參數Create.NewFamilyInstance是基於level
+            //因此在這裡做修正，扣掉level的elevation
+            XYZ targetPt = new XYZ(ptClosest.X, ptClosest.Y, ptClosest.Z - lev.Elevation);
+            #endregion
+
+
+            //結果：Vetor待調整
+
+            ////////////////////////////////////////////////////////////////////////////////////////////
+
+            #region 創建吊架
             using (Transaction transAct = new Transaction(doc))
             {
                 transAct.Start("Create standard-alone instance");
@@ -157,7 +161,7 @@ namespace MAutoHangerCreation
                     famSym.Activate();
                 }
 
-                FamilyInstance famIns = doc.Create.NewFamilyInstance(pt2, famSym, lev, Autodesk.Revit.DB.Structure.StructuralType.NonStructural);
+                FamilyInstance famIns = doc.Create.NewFamilyInstance(targetPt, famSym, lev, Autodesk.Revit.DB.Structure.StructuralType.NonStructural);
                 transAct.Commit();
 
                 st.AppendLine("新增了一個吊架：");
@@ -165,13 +169,13 @@ namespace MAutoHangerCreation
                 MessageBox.Show(st.ToString());
                 st.Clear();
             }
-
-
+            #endregion
             return Result.Succeeded;
         }
 
 
 
+        #region 這裡的Filter是for Selection
         public class PipeSelFilter : ISelectionFilter
         {
             Document docDefault = null;
@@ -188,10 +192,10 @@ namespace MAutoHangerCreation
                 Category conduit = Category.GetCategory(docDefault, BuiltInCategory.OST_Conduit);
                 Category tray = Category.GetCategory(docDefault, BuiltInCategory.OST_CableTray);
 
-                if (eForFil.Category.Id == pipe.Id      ||
-                    eForFil.Category.Id == duct.Id      ||
-                    eForFil.Category.Id == conduit.Id   ||
-                    eForFil.Category.Id == tray.Id )
+                if (eForFil.Category.Id == pipe.Id ||
+                    eForFil.Category.Id == duct.Id ||
+                    eForFil.Category.Id == conduit.Id ||
+                    eForFil.Category.Id == tray.Id)
                     return true;
                 else
                     return false;
@@ -202,6 +206,7 @@ namespace MAutoHangerCreation
                 return true;
             }
         }
+        #endregion    
+        }
     }
-}
 
